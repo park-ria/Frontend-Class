b7cb3d225a0a0e6068e473e5f7ad992edcc57310
park-ria <danny6217@gmail.com>
2024-07-05 17:50:50 +0900

    240705

--- profile project/script.js
+++ profile project/script.js
@@ -1,15 +1,1 @@
-const mainTitleTxt = document.querySelector("#main_title_txt");
-const txt = "Hello, Ria world!";
-let txtIndex = 0;
 
-// typing 함수
-let typing = function () {
-  mainTitleTxt.innerHTML += txt[txtIndex];
-  txtIndex++;
-  if (txtIndex > txt.length) {
-    mainTitleTxt.textContent = "";
-    txtIndex = 0;
-  }
-};
-
-setInterval(typing, 200);
