f48d535dca32610848443832a060d9239c441ea2
park-ria <danny6217@gmail.com>
2024-07-17 18:18:55 +0900

    240717

--- 240715/02flex/script.js
+++ 240715/02flex/script.js
@@ -1,10 +1,69 @@
+// Header Nav
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
   const header = document.querySelector("header");
+  const gototop = document.querySelector(".gototop");
 
   if (scroll > 50) {
     header.classList.add("active");
+    gototop.classList.add("active");
   } else {
     header.classList.remove("active");
+    gototop.classList.remove("active");
   }
+});
+
+// Trigger
+const trigger = document.querySelector(".trigger");
+const gnb = document.querySelector(".gnb");
+const gnbLinks = gnb.querySelectorAll("a");
+
+trigger.addEventListener("click", function () {
+  const gnb = document.querySelector(".gnb");
+  this.classList.toggle("active");
+  gnb.classList.toggle("active");
+});
+
+gnbLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    trigger.classList.remove("active");
+    gnb.classList.remove("active");
+  });
+});
+
+// Slick Slider
+$(".myslider").slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+  ],
 });
