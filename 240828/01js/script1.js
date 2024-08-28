35aed31339dcd2526c5c69610a8f4c8f1c5d291c
park-ria <danny6217@gmail.com>
2024-08-28 16:34:17 +0900

    240828

---
+++ 240828/01js/script1.js
@@ -0,0 +1,26 @@
+const canvas = document.querySelector("canvas");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const ctx = canvas.getContext("2d");
+
+// const img = new Image();
+// /*img.addEventListener("load", () => {
+//   ctx.drawImage(img, 0, 0);
+// });*/
+// img.onload = function () {
+//   //ctx.drawImage(img, 0, 0, 300, 200);
+//   //ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+//   ctx.drawImage(img, 100, 50, 280, 350, 160, 100, 140, 175);
+// };
+// img.src = "./canvasAPI/cat.jpg";
+
+const img = new Image();
+img.onload = function () {
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+};
+img.src = "./canvasAPI/bird.jpg";
+
+ctx.beginPath();
+ctx.arc(480, 380, 150, 0, Math.PI * 2, false);
+ctx.clip();
---
+++ 240828/01js/script4.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector("canvas");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const ctx = canvas.getContext("2d");
+
+// 선의 굵기 && 선의 끝부분 처리
+// lineWidth = "" => 선의 굵기
+// lineCap = butt(*기본값) // round // square
+
+// const lineCap = ["butt", "round", "square"];
+// for (let i = 0; i < lineCap.length; i++) {
+//   ctx.beginPath();
+//   ctx.moveTo(50, 50 + i * 30);
+//   ctx.lineTo(350, 50 + i * 30);
+//   ctx.lineWidth = 15;
+//   ctx.lineCap = lineCap[i];
+//   ctx.stroke();
+// }
+//lineCap의 round와 square는 전체 lineWidth의 절반값은 7.5만큼 증가해서 끝을 둥글게, 끝을 사각으로 만듬
+
+// 선이 교차하는 지점에서의 효과처리
+// bevel // miter // round
+
+const lineJoin = ["bevel", "miter", "round"];
+for (let i = 0; i < lineJoin.length; i++) {
+  ctx.beginPath();
+  ctx.moveTo(60, 60 * i + 50);
+  ctx.lineTo(100, 60 * i + 15);
+  ctx.lineTo(140, 60 * i + 50);
+  ctx.lineWidth = 20;
+  ctx.lineJoin = lineJoin[i];
+  ctx.stroke();
+}
