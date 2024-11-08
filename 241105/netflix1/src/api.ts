99ae8c2954d609c8d2b42b1393499a4ee9531a94
park-ria <danny6217@gmail.com>
2024-11-08 15:57:51 +0900

    241108

---
+++ 241105/netflix1/src/api.ts
@@ -0,0 +1,44 @@
+const API_KEY = "10cb8d48525789ecadc68d2dc2035480";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface Movie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  original_title: string;
+  overview: string;
+  vote_average: number;
+  vote_count: number;
+  release_date: string;
+  adult: boolean;
+}
+
+export interface GetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export const getMovies = () => {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+};
+
+export const searchContents = (keyword: string | null) => {
+  return fetch(
+    `${BASE_PATH}/search/multi?api_key=${API_KEY}&query=${keyword}`
+  ).then((response) => response.json());
+};
+
+export const searchGeners = () => {
+  return fetch(`${BASE_PATH}/genre/movie/list?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+};
